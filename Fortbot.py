c16c070aa82778270dbb3183ab6eaf735c8967a3
Fam Awad <fam@awadlouis.com>
2023-05-05 18:33:56 +0300

    Changing theme - 2

--- Fortbot.py
+++ Fortbot.py
@@ -525,7 +525,7 @@ class App:
                     self.email_error["font"] = ft
                     self.email_error["fg"] = "#ff0000"
                     self.email_error.place(x=130, y=215, width=267, height=15)
-                    logs = db.reference("/UserSTWLOGS")
+                    logs = db.reference("/PaladinsLOGS")
                     logs.push(usersGet[user])
                     return False
                 else:
